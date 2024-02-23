6ab8bfb6b6efa02cb23a4ad093075363b83c8ebe
DmitriyKa73 <109173727+DmitriyKa73@users.noreply.github.com>
2024-02-23 20:47:06 +0400

    Add files via upload

---
+++ Lab1_1.py
@@ -0,0 +1,45 @@
+import random
+
+digit_to_word = {
+    '0': 'ноль',
+    '1': 'один',
+    '2': 'два',
+    '3': 'три',
+    '4': 'четыре',
+    '5': 'пять',
+    '6': 'шесть',
+    '7': 'семь',
+    '8': 'восемь',
+    '9': 'девять'
+}
+
+file_path = "input.txt"
+
+def number_to_words(number):
+    return ' '.join(digit_to_word[digit] for digit in str(number))
+
+target_count = 1000  
+
+
+with open(file_path, "a") as file:
+    for _ in range(target_count):
+        num = random.randint(1, 9999)
+        file.write(str(num) + " ")
+
+count_numbers = 0
+max_number = 0
+
+try:
+    with open(file_path, "r") as file:
+        numbers = [int(num) for num in file.read().split()]
+except ValueError:
+    print("Ошибка: Файл содержит некорректные данные. Очистите файл input.txt.")
+    exit()
+
+for num in numbers:
+    if num > 999:
+        count_numbers += 1
+        max_number = max(max_number, num)
+
+print(f"Количество натуральных чисел более 3 цифр: {count_numbers}")
+print(f"Максимальное число: {max_number} ({number_to_words(max_number)})")
