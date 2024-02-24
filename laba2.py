1a4de5dd9aa9c924c9024e46a26b2c5bab8b8233
DmitriyKa73 <109173727+DmitriyKa73@users.noreply.github.com>
2024-02-24 12:37:35 +0400

    Update laba2.py

--- laba2.py
+++ laba2.py
@@ -1,8 +1,10 @@
 #ЗАДАНИЕ: Написать программу, которая читая символы из бесконечной последовательности (эмулируется конечным файлом, читающимся поблочно), распознает, преобразует и выводит на экран лексемы по определенному правилу.
 #Лексемы разделены пробелами. Преобразование делать по возможности через словарь. Для упрощения под выводом числа прописью подразумевается последовательный вывод всех цифр числа. Регулярные выражения использовать нельзя.
 #ВАРИАНТ 12. Натуральные числа, содержащие более 3 цифр. Вывести количество таких чисел. Максимальное число вывести прописью.
-import random
 import re
+
+file_path = "input.txt"
+
 digit_to_word = {
     '0': 'ноль',
     '1': 'один',
@@ -16,27 +18,35 @@ digit_to_word = {
     '9': 'девять'
 }
 
-file_path = "input.txt"
 
 def number_to_words(number):
     return ' '.join(digit_to_word[digit] for digit in str(number))
 
+
 count_numbers = 0
 max_number = 0
+current_number = ""
 
 try:
     with open(file_path, "r") as file:
-        block_size = 1024 
-        pattern = r'\b\d{1,4}\b'  
+        block_size = 1024
+        pattern = r'\b\d+\b' 
 
         block = file.read(block_size)
         while block:
-            numbers = [int(match.group()) for match in re.finditer(pattern, block)]
+            current_number += block
+            matches = re.finditer(pattern, current_number)
 
-            for num in numbers:
+            match = next(matches, None) 
+            while match:
+                num = int(match.group())
                 if num > 999:
                     count_numbers += 1
                     max_number = max(max_number, num)
+                match = next(matches, None)  
+
+           
+            current_number = current_number[match.end():] if match else ""
 
             block = file.read(block_size)
 
