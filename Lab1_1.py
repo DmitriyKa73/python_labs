cf5cfdcc8f02cc785c10a390f6d02ccff69cf535
DmitriyKa73 <109173727+DmitriyKa73@users.noreply.github.com>
2024-02-24 13:04:22 +0400

    Update Lab1_1.py

--- Lab1_1.py
+++ Lab1_1.py
@@ -1,5 +1,3 @@
-import random
-
 digit_to_word = {
     '0': 'ноль',
     '1': 'один',
@@ -12,34 +10,28 @@ digit_to_word = {
     '8': 'восемь',
     '9': 'девять'
 }
-
 file_path = "text.txt"
 
 def number_to_words(number):
     return ' '.join(digit_to_word[digit] for digit in str(number))
-
 count_numbers = 0
 max_number = 0
-
 try:
     with open(file_path, "r") as file:
         block_size = 1024
         block = file.read(block_size)
 
-        while block:
-           
+        while block:        
             numbers = [int(num) for num in block.split() if num.isdigit() and int(num) > 999]
 
             for num in numbers:
                 if num > 999:
                     count_numbers += 1
                     max_number = max(max_number, num)
-
             block = file.read(block_size)
 
 except ValueError:
     print("Ошибка: Файл содержит некорректные данные. Очистите файл input.txt.")
     exit()
-
 print(f"Количество натуральных чисел более 3 цифр: {count_numbers}")
 print(f"Максимальное число: {max_number} ({number_to_words(max_number)})")
