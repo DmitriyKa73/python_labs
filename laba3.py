781756f05e3a0103d2c82adabf044e16fa468a7e
DmitriyKa73 <109173727+DmitriyKa73@users.noreply.github.com>
2024-03-09 10:47:15 +0400

    Update laba3.py

--- laba3.py
+++ laba3.py
@@ -2,70 +2,105 @@
 #12.	Формируется матрица F следующим образом: если в В количество простых чисел в нечетных столбцах в области 2 больше,
 # чем сумма чисел в четных строках в области 1, то поменять в Е симметрично области 1 и 2 местами, иначе С и Е поменять местами несимметрично.
 # При этом матрица А не меняется. После чего вычисляется выражение: ((К*A)*А– K*AT . Выводятся по мере формирования А, F и все матричные операции последовательно.
-
 import random
 def is_prime(num):
     if num < 2:
         return False
-    for i in range(2, int(num ** 0.5) + 1):
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
     return True
 
-def generate_matrix():
-    return [[random.randint(-10, 10) for _ in range(2)] for _ in range(2)]
+def create_matrix(N):
+    return [[random.randint(-10, 10) for _ in range(N)] for _ in range(N)]
 
-def count_primes(matrix):
-    return sum(is_prime(num) for num in matrix)
+def count_primes_in_odd_columns(B):
+    count = 0
+    for row in B:
+        for i in range(0, len(row), 2):  # Нечетные столбцы (с индексом 0, 2, ...)
+            if is_prime(row[i]):
+                count += 1
+    return count
 
-def create_F_matrix(matrix_B):
-    F = [row.copy() for row in matrix_B]
-    area_1_sum = sum(F[i][j] for i in range(2) for j in range(2))
-    area_2_primes = count_primes([F[i][1] for i in range(2)])
+def sum_in_even_rows(C):
+    total = 0
+    for i in range(1, len(C), 2):  # Четные строки (с индексом 1, 3, ...)
+        total += sum(C[i])
+    return total
 
-    if area_2_primes > area_1_sum:
-        F[0][:2], F[1][:2] = F[1][::-1][:2], F[0][::-1][:2]
-        F[2][:2], F[3][:2] = F[3][::-1][:2], F[2][::-1][:2]
-    else:
-        F[0][:2] = F[0][:2][::-1]
-        F[1][:2] = F[1][:2][::-1]
+def transpose_matrix(matrix):
+    return [list(row) for row in zip(*matrix)]
 
-    return F
+def multiply_matrix_by_scalar(matrix, scalar):
+    return [[scalar * cell for cell in row] for row in matrix]
 
-def get_integer_input(prompt):
-    while True:
-        try:
-            value = int(input(prompt))
-            return value
-        except ValueError:
-            print("Ошибка! Введите целое число.")
+def subtract_matrices(A, B):
+    return [[A[i][j] - B[i][j] for j in range(len(A[i]))] for i in range(len(A))]
+
+def matrix_multiplication(A, B):
+    result = [[0 for _ in range(len(B[0]))] for _ in range(len(A))]
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                result[i][j] += A[i][k] * B[k][j]
+    return result
+
+def print_matrix(matrix):
+    for row in matrix:
+        print(' '.join(map(str, row)))
+    print()
 
 def main():
-    K = get_integer_input("Введите число K: ")
-    N = get_integer_input("Введите число N: ")
+    K = int(input("Введите число K: "))
+    N = int(input("Введите размерность матрицы N: "))
 
-    A = [[random.randint(-10, 10) for _ in range(N)] for _ in range(N)]
-    B = generate_matrix()
-    C = generate_matrix()
-    D = generate_matrix()
-    E = generate_matrix()
+    if N % 2 != 0:
+        print("N должно быть четным.")
+        return
 
-    F = create_F_matrix(B)
+    A = create_matrix(N)
+    print("Матрица A:")
+    print_matrix(A)
 
-    result = [[K * A[i][j] * A[i][k] - K * A[k][j] for k in range(N)] for i in range(N) for j in range(N)]
+    mid = N // 2
+    B = [row[:mid] for row in A[:mid]]
+    C = [row[mid:] for row in A[:mid]]
+    D = [row[:mid] for row in A[mid:]]
+    E = [row[mid:] for row in A[mid:]]
 
-    print("\nМатрица A:")
-    for row in A:
-        print(row)
+    if count_primes_in_odd_columns(B) > sum_in_even_rows(C):
+        for i in range(mid):
+            for j in range(mid):
+                E[i][j], E[j][i] = E[j][i], E[i][j]
+    else:
+        C, E = E, C
 
-    print("\nМатрица F:")
-    for row in F:
-        print(row)
+    F = [B[i] + C[i] for i in range(mid)] + [D[i] + E[i] for i in range(mid)]
 
-    print("\nРезультат выражения ((К*A)*А– K*A^T):")
-    for row in result:
-        print(row)
+    print("Матрица F после преобразований:")
+    print_matrix(F)
 
+    KA = multiply_matrix_by_scalar(A, K)
+    A_transposed = transpose_matrix(A)
+    KA_transposed = multiply_matrix_by_scalar(A_transposed, K)
+    A_squared = matrix_multiplication(A, A)
+    KAA_minus_KAT = subtract_matrices(multiply_matrix_by_scalar(A_squared, K), KA_transposed)
+
+    # Вывод результатов
+    print("Матрица KA (K умноженное на A):")
+    print_matrix(KA)
+
+    print("Матрица A транспонированная:")
+    print_matrix(A_transposed)
+
+    print("Матрица KAT (K умноженное на A транспонированное):")
+    print_matrix(KA_transposed)
+
+    print("Матрица KAA (K умноженное на A квадрат):")
+    print_matrix(multiply_matrix_by_scalar(A_squared, K))
+
+    print("Результат выражения ((К*A)A– KA^T):")
+    print_matrix(KAA_minus_KAT)
 
 if __name__ == "__main__":
     main()
